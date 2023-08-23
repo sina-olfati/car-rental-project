0c7ba2e8b824d1558594e1bfd06790206754965b
sina <sinaolfati5@gmail.com>
2023-08-23 11:31:34 +0330

    book system UI | plan trip section responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,17 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import BookCar from "./BookCar";
+import PlanTrip from "./PlanTrip";
+
 function Home() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <BookCar />
+      <PlanTrip />
+    </>
+  );
 }
 
 export default Home;
